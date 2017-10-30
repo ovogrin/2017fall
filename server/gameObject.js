4f2e8e1845069d3645eaf3a34cf493a4d6b7000a
Otto Vogrincic <ovogrin@gmail.com>
2017-10-30 10:39:33 -0400

    Added server-side function to serve the quote and picture data

--- server/gameObject.js
+++ server/gameObject.js
@@ -1,12 +1,22 @@
-module.exports = {
+let iCurrentPicture = 0;
+let iCurrentQuote = 0;
+
+const game = {
     pictures: [
         "http://tse4.mm.bing.net/th?id=ORT.TH_470633631&pid=1.12&eid=G.470633631",
         "https://www.w3schools.com/css/trolltunga.jpg"
     ],
-
+    getNextPicture: () => game.pictures[iCurrentPicture++],
     quotes: [
         { text: "Get me out of here"},
         { text: "They're just fine"},
         { text: "Anywhere but Hasbrouck"},
-    ]
+    ],
+    room: {
+        picture: "",
+        quotes: []
+    }
+
 }
+
+module.exports = game;
